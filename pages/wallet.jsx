156e4a2a51d737dc409a7e2ac37f175d7a308057
Qozeem Salami <qozeemopeyemi2008@gmail.com>
2022-08-31 22:16:05 +0100

    New Commit

--- pages/wallet.jsx
+++ pages/wallet.jsx
@@ -1,10 +1,12 @@
 // import Pools from "../components/Pools";
-import Wallets from "../components/BranchWallet/tokenWallet";
-import NewSendTokens from "../components/BranchWallet/NewSendTokens";
+// import Wallets from "../components/BranchWallet/tokenWallet";
+// import NewSendTokens from "../components/BranchWallet/NewSendTokens";
+// import ExchangeTokens from "../components/BranchWallet/ExchangeTokens";
+import NewAddLiquidity from "../components/BranchWallet/NewAddLiquidity";
 // import NewPool from "../components/BranchWallet/NewPool";
 // import AddLiquidity from "../components/BranchWallet/AddLiquidity"
-import CircleList from "../components/BranchWallet/CircleList";
-import NewProfile from "../components/Profiles/NewProfile";
+// import CircleList from "../components/BranchWallet/CircleList";
+// import NewProfile from "../components/Profiles/NewProfile";
 
 const WalletIndex = () => {
   const logo = "../aerx_logo-removebg-preview 1 (Traced)-1.svg";
@@ -16,8 +18,10 @@ const WalletIndex = () => {
       {/* <AddLiquidity /> */}
       {/* <CircleList /> */}
       {/* <NewProfile /> */}
-      <NewSendTokens />
+      {/* <NewSendTokens /> */}
       {/* <NewPool /> */}
+      {/* <ExchangeTokens /> */}
+      <NewAddLiquidity />
     </div>
   );
 };
